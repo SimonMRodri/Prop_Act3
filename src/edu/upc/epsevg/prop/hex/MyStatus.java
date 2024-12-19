76337e6a211e8a12f82848c3c88906b96abc0a78
SimonMRodri <simon.moreno@estudiantat.upc.edu>
2024-12-19 11:59:19 +0100

    Auto stash before merge of "main" and "origin/main"
    gethashcode

--- src/edu/upc/epsevg/prop/hex/MyStatus.java
+++ src/edu/upc/epsevg/prop/hex/MyStatus.java
@@ -1,6 +1,8 @@
 
 package edu.upc.epsevg.prop.hex;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MyStatus extends HexGameStatus {
     
     public HexGameStatus hgs;
@@ -18,6 +20,23 @@ public class MyStatus extends HexGameStatus {
     
     public int getHeuristica(){
         return heur;
+    }
+    
+    public long getHashCode(int x, int y){
+        int n = this.getSize();
+        long[][][] zobrist = new long[n][n][3];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k < n; k++){
+                    zobrist[i][j][k] = ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
+                }    
+            }
+        }
+        long hash = 0;
+        
+        int valor = this.getPos(x, y);
+        hash ^= zobrist[x][y][valor+1];
+        return hash;
     }
     
 }
